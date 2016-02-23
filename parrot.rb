3af85f667755a4e7ed295eb4377321b48b563422
abarranco650 <abarranco650@yahoo.com>
2016-02-23 15:40:33 -0800

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
+
+def parrot(repeat = "Squawk!")
+  puts "#{repeat}"
+  return repeat
+
+end
+
+parrot("Pretty Bird!")
+
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase

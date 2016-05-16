b2e9fea4444c66e32282a221eebe08a1b0df8e41
6ytrenches <myers.m007@gmail.com>
2016-05-15 20:30:55 -0400

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,6 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(bird = "Squawk!")
+  puts bird
+   phrase = bird
+
+  end# Create method `parrot` that outputs a given phrase and
 # returns the phrase
